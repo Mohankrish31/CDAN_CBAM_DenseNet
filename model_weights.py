591f028e4c5bfc49ae1b1c56baeb0b9032884978
Mohankrish31 <134041145+Mohankrish31@users.noreply.github.com>
2025-09-23 16:33:50 +0530

    Update model_weights.py

--- model_weights.py
+++ model_weights.py
@@ -3,7 +3,7 @@ from models.cdan_cbam_densenet import CDAN_CBAM_DenseNet
 import os
 # Setup
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = CDAN_CBAM_DenseNet(self, in_channels=3, out_channels=3, base_channels=64).to(device)
+model = CDAN_CBAM_DenseNet(in_channels=3, out_channels=3, base_channels=64).to(device)
 # Training code here...
 # After training, save weights:
 os.makedirs("/content/saved_model", exist_ok=True)
